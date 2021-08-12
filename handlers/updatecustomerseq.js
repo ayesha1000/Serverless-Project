eceac44e2fc07e67fadb26f6d25ecd1c457256b9
Ayesha Munir <amunir.bscs17seecs@seecs.edu.pk>
2021-08-12 12:46:02 +0500

    third commit

--- handlers/updatecustomerseq.js
+++ handlers/updatecustomerseq.js
@@ -11,17 +11,18 @@ module.exports.handler = async(event) => {
     const {Customer} = await connectToDatabase();
     const customers = await Customer.findAll({ attributes: ['customer_id', 'customer_name','customer_phoneno','customer_address'], 
     where: {deletflag: 0, customer_id: event.pathParameters.id}})
+    let name = JSON.stringify(customers[0].customer_name)
+    let address = JSON.stringify(customers[0].customer_address)
+    let phoneno = JSON.stringify(customers[0].customer_phoneno)
+    
 
-    let custom = {customer_id :JSON.stringify(customers[0].customer_id), customer_name : JSON.stringify(customers[0].customer_name) ,
-    customer_phoneno : JSON.stringify(customers[0].customer_phoneno), customer_address : JSON.stringify(customers[0].customer_address)}
+    let custom = {customer_id :JSON.stringify(customers[0].customer_id), customer_name : name.substring(1, name.length-1) ,
+    customer_phoneno : phoneno.substring(1, phoneno.length-1), customer_address : address.substring(1, address.length-1)}
     const {customer_id,customer_name,customer_phoneno, customer_address} = {...custom,...JSON.parse(event.body)}
-    console.log(customer_id)
-    console.log(customer_phoneno)
-    console.log(customer_address)
-    console.log(customer_name)
-
+    
     await Customer.update({customer_id: customer_id, customer_name: customer_name, customer_phoneno: customer_phoneno, customer_address: customer_address},
         {where: {customer_id: event.pathParameters.id}})
+
     if(!customers) {
       throw new HTTPError(404,'Could not create Customer')
     }
